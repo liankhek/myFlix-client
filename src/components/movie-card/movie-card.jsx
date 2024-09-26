e7ef404a42d20660cc6aad0d15e9ecc90cb6a96a
liankhek <liankhek@gmail.com>
2024-09-26 17:39:43 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div onClick={() => onMovieClick(movie)} style={{ cursor: 'pointer' }}>
       <div>{movie.title}</div>
@@ -11,11 +11,10 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    imageUrl: PropTypes.string,
-    genre: PropTypes.string,
-    director: PropTypes.string,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
+
+// Export as default
+export default MovieCard;
