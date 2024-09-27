25539842d5772ee47db7598e515ddebb90065cfd
liankhek <liankhek@gmail.com>
2024-09-27 16:38:43 -0500

    Update main-view.jsx
    Add Login and Signup

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,30 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import MovieCard from '../movie-card/movie-card';
 import MovieView from '../movie-view/movie-view';
+import LoginView from '../login-view/login-view';
+import { SignupView } from "../signup-view/signup-view";
+
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(storedUser);
+  const [token, setToken] = useState(storedToken);
 
   useEffect(() => {
+    if (!token) return;
     // Fetch movies data from the API
-    fetch('https://da-flix-1a4fa4a29dcc.herokuapp.com/movies')
-      .then((response) => response.json())
+    fetch('https://da-flix-1a4fa4a29dcc.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
       .then((data) => {
         const moviesFromApi = data.map((movie) => ({
           _id: movie._id,
           Title: movie.Title,
           Description: movie.Description,
-          Genre: movie.Genre,
-          Director: movie.Director,
+          genre: movie.genre?.name || "Unknown Genre",
+          directors: movie.directors?.[0]?.name || "Unknown Director",
           ImagePath: movie.ImagePath,
           Featured: movie.Featured,
         }));
         setMovies(moviesFromApi);
       })
       .catch((error) => console.error('Error fetching movies:', error));
-  }, []); // Empty dependency array to ensure it runs once
-  
+  }, [token]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     const similarMovies = movies.filter(
@@ -56,6 +90,15 @@ export const MainView = () => {
           />
         </div>
       ))}
+      <button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
