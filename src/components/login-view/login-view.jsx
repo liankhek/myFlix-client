60f24d951b29bedaada6c2c583b37c20cfbbc960
liankhek <liankhek@gmail.com>
2024-09-28 13:08:38 -0500

    Update login-view.jsx
    use bootstrap

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState('');
@@ -35,24 +37,32 @@ export const LoginView = ({ onLoggedIn }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-            type="text" 
-            value={username} onChange={(e) => setUsername(e.target.value)} required 
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          placeholder="Enter your username"
         />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input 
-            type="password" 
-            value={password} onChange={(e) => setPassword(e.target.value)} required 
+      </Form.Group>
+  
+      <Form.Group controlId="formPassword" className="mt-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          placeholder="Enter your password"
         />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-    </form>
+      </Form.Group>
+  
+      <Button variant="primary" type="submit" className="mt-3">
+        Login
+      </Button>
+    </Form>
   );
 };
