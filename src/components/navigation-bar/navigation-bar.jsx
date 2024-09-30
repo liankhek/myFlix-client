892b033d19c9148c6082c817a905b149f2f19bda
liankhek <liankhek@gmail.com>
2024-09-30 17:20:04 -0500

    Update

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,61 +1,23 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
-  const navigate = useNavigate();
-
-  const handleProfileClick = (event) => {
-    event.preventDefault(); // Prevent the default link behavior
-    navigate(`/users/${encodeURIComponent(user._id)}`); // Navigate to the profile route
-    window.location.reload(); // Reload the page to ensure fresh data
-  };
-
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand
-          as={Link}
-          to="/" // Home route
-        >
-          MyFlixDB
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {user && user._id && (
-              <>
-                <Nav.Link
-                  as={Link}
-                  to={`/users/${encodeURIComponent(user._id)}`}
-                  onClick={handleProfileClick}
-                >
-                  Profile
-                </Nav.Link>
-                <Nav.Link
-                  as={Link}
-                  to="/"
-                  onClick={() => {
-                    onLoggedOut();
-                    navigate("/login");
-                  }}
-                >
-                  Log Out
-                </Nav.Link>
-              </>
-            )}
-            {!user && (
-              <>
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to="/signup">
-                  Signup
-                </Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/">MyFlixDB</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          {user && (
+            <>
+              <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
+              <Nav.Link as={Link} to={`/users/${user._id}`}>Profile</Nav.Link>
+              <Nav.Link onClick={onLoggedOut}>Log Out</Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
