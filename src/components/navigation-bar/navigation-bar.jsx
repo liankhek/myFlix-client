364d13cd3c1dedea555b93990adcbb65732ee28d
liankhek <liankhek@gmail.com>
2024-09-30 17:52:55 -0500

    Update

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -8,15 +8,13 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
       <Navbar.Brand as={Link} to="/">MyFlixDB</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          {user && (
-            <>
-              <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
-              <Nav.Link as={Link} to={`/users/${user._id}`}>Profile</Nav.Link>
-              <Nav.Link onClick={onLoggedOut}>Log Out</Nav.Link>
-            </>
-          )}
-        </Nav>
+        {user && (
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/">Movies</Nav.Link>
+            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+            <Nav.Link onClick={onLoggedOut}>Log Out</Nav.Link>
+          </Nav>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
